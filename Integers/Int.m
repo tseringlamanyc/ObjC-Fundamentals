e601447c2f693d5d5c18f3d0ede62dc3b792a937
Tsering Lama <tseringlama@pursuit.org>
2020-04-27 14:21:43 -0400

    number notes added

--- Integers/Int.m
+++ Integers/Int.m
@@ -8,11 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+/*
+ int is a series of 1s and 0s that represent 32-bit numbers
+ NSInteger is a series of 1s and 0s that can be either a 32-bit integer or a 64-bit integer depending on the CPU
+ NSNumber for storing numbers (its an object)
+ NSInteger holds only integer
+ NSNumber can hold int, NSInteger, floats, doubles, and even booleans
+ NSNumber: a class that serves to wrap each of those number types in an object so that we can put them into arrays, dictionaries, sets, and other places that require objects
+ */
+
 int integers(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        // MARK: NSNumber
+        NSNumber *intergerTen = @10;
+        NSNumber *boolTrue = @YES;
+        NSNumber *doublePI = @M_PI;
         
-        NSLog(@"Int");
     }
     return 0;
 }
